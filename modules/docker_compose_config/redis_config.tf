23ba99c41ebb51d486cdca687162f48490e64bc5
Arnvid Karstad <arnvid@opslab.cloud>
2023-11-02 23:33:47 +0100

    add: redis check for port number in host string

--- modules/docker_compose_config/redis_config.tf
+++ modules/docker_compose_config/redis_config.tf
@@ -2,8 +2,9 @@
 # SPDX-License-Identifier: MPL-2.0
 
 locals {
+  tfe_redis_port_present = strcontains(var.redis_host, ":")
   redis = {
-    TFE_REDIS_HOST     = var.redis_use_tls != null ? var.redis_use_tls ? "${var.redis_host}:6380" : var.redis_host : null
+    TFE_REDIS_HOST     = local.tfe_redis_port_present ? var.redis_host :  var.redis_use_tls != null ? var.redis_use_tls ? "${var.redis_host}:6380" : var.redis_host : null
     TFE_REDIS_USER     = var.redis_user
     TFE_REDIS_PASSWORD = var.redis_password
     TFE_REDIS_USE_TLS  = var.redis_use_tls
